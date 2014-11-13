13efcfef583d5f65890ac7eaf677b120f1cc5d85
Patrick Wright <patrick@getchef.com>
2014-11-13 13:37:45 -0700

    update knife.rb to use config_dir

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,4 +1,5 @@
-current_dir = File.dirname(__FILE__)
-chef_repo_path File.join(current_dir, '..')
-cookbook_path [ File.join(current_dir, '..', 'cookbooks'),
-                File.join(current_dir, '..', 'berks-cookbooks') ]
+config_dir File.dirname(__FILE__)
+chef_repo_path File.join(config_dir, '..')
+cookbook_path [ File.join(config_dir, '..', 'cookbooks'),
+                File.join(config_dir, '..', 'berks-cookbooks') ]
+cache_path File.join(config_dir, 'local-mode-cache')
